13aa82897549c309829c26e39b21a2dec5ced729
sea-yjd <74958169+sea-yjd@users.noreply.github.com>
2023-03-07 17:03:41 +0800

    Create 26_.py

---
+++ 26_.py
@@ -0,0 +1,20 @@
+# [26] 删除有序数组中的重复项
+#
+
+# @lc code=start
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+        slow = 0
+        fast = 1
+        while fast < len(nums):
+            if nums[slow] != nums[fast]:
+                slow += 1
+                nums[slow] = nums[fast]
+            fast += 1
+        return slow + 1
