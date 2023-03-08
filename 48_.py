228af1d5d8cb87fa3954d5f9a5c43cb6f05a706c
sea-yjd <74958169+sea-yjd@users.noreply.github.com>
2023-03-08 17:27:11 +0800

    Update 48_.py

--- 48_.py
+++ 48_.py
@@ -14,6 +14,6 @@ class Solution(object):
         for i in range(n):
             for j in range(i, n):
                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
-        for row in matrix:
-            row = row[::-1]
+        for i in range(n):
+            matrix[i] = matrix[i][::-1]
         return matrix
