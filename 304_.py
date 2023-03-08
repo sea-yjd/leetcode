1314938399345f0316b61a1e82c2d4868bfd48d8
sea-yjd <74958169+sea-yjd@users.noreply.github.com>
2023-03-08 15:51:46 +0800

    Create 304_.py

---
+++ 304_.py
@@ -0,0 +1,27 @@
+# [304] 二维区域和检索 - 矩阵不可变
+#
+# 前缀和矩阵
+# @lc code=start
+class NumMatrix(object):
+
+    def __init__(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        """
+        m = len(matrix)
+        n = len(matrix[0])
+        if m==0 or n==0:return
+        self.preSum = [[0 for _ in range(n+1)] for _ in range(m+1)]
+        for i in range(1, m+1):
+            for j in range(1, n+1):
+                self.preSum[i][j] = self.preSum[i-1][j] + self.preSum[i][j-1] - self.preSum[i-1][j-1] + matrix[i-1][j-1]
+
+    def sumRegion(self, row1, col1, row2, col2):
+        """
+        :type row1: int
+        :type col1: int
+        :type row2: int
+        :type col2: int
+        :rtype: int
+        """
+        return self.preSum[row2+1][col2+1] - self.preSum[row2+1][col1] - self.preSum[row1][col2+1] + self.preSum[row1][col1]
