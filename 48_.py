ef9ebd586e7178679b57c4b0523c1b3c0267982a
sea-yjd <74958169+sea-yjd@users.noreply.github.com>
2023-03-08 17:23:42 +0800

    Create 48_.py

---
+++ 48_.py
@@ -0,0 +1,19 @@
+# @lc app=leetcode.cn id=48 lang=python
+#
+# [48] 旋转图像
+#
+# 顺时针旋转：沿着主对角线镜像交换，之后每一行的元素翻转
+# @lc code=start
+class Solution(object):
+    def rotate(self, matrix):
+        """
+        :type matrix: List[List[int]]
+        :rtype: None Do not return anything, modify matrix in-place instead.
+        """
+        n = len(matrix)
+        for i in range(n):
+            for j in range(i, n):
+                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        for row in matrix:
+            row = row[::-1]
+        return matrix
