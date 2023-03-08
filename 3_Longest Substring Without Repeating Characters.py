59f61671a0fa089e8912b628b3e5012c5454d960
sea-yjd <74958169+sea-yjd@users.noreply.github.com>
2023-03-08 10:23:42 +0800

    Update 3_Longest Substring Without Repeating Characters.py

--- 3_Longest Substring Without Repeating Characters.py
+++ 3_Longest Substring Without Repeating Characters.py
@@ -13,3 +13,31 @@ class Solution:
                 dict[s[i]] = i
                 if max < (i - start): max = i-start
         return max
+    
+ # [3] 无重复字符的最长子串
+#
+# 解法二：使用滑动窗口套路
+# @lc code=start
+from collections import defaultdict
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        window = defaultdict(int)
+        left, right = 0, 0
+        length = 0
+        while right < len(s):
+            c = s[right]
+            if c not in window or window[c] == 0:
+                window[c] += 1
+                right += 1
+                if right - left > length:
+                    length = right - left
+            else:
+                # right停止
+                d = s[left]
+                window[d] -= 1
+                left += 1
+        return length
