cac13e05cf1f0d972632fb781d093f997dbef7b9
sea-yjd <74958169+sea-yjd@users.noreply.github.com>
2023-03-13 11:22:37 +0800

    Update 116.py

--- 116.py
+++ 116.py
@@ -18,29 +18,30 @@ class Solution(object):
       self.right2left(root.left, root.right)
       return root
     
-  # 解法二：宽度遍历 -----  leetcode结果是错的，问题出在哪里？
-  def connect(self, root):
-      """
-      :type root: Node
-      :rtype: Node
-      """
-      if (root == None):
-          return root
-      que = []
-      que.append(root)
-      while (que):
-          size = len(que)
-          prev = None
-          while size > 0:
-              node = que.pop()
-              if (prev != None):
-                  prev.next = node
-              if node.left != None:
-                  que.append(node.left)
-              if node.right != None:
-                  que.append(node.right)
-              prev = node
-              size -= 1
-      return root
+  # 解法二：宽度遍历 -- 这里注意：宽度遍历需要队列，而list可以实现堆栈的append()和pop()，对于队列需要用到deque：append()/pop()都是对对尾进行操作，appendleft()
+  # 和popleft()都是对对头操作。其中用que的长度size控制搜索的范围，设计巧妙
+    def connect(self, root):
+        """
+        :type root: Node
+        :rtype: Node
+        """
+        if (root == None):
+            return root
+        que = deque()
+        que.append(root)
+        while (len(que)>0):
+            size = len(que)
+            prev = None
+            while size > 0:
+                node = que.popleft()
+                if (prev != None):
+                    prev.next = node
+                if node.left != None:
+                    que.append(node.left)
+                if node.right != None:
+                    que.append(node.right)
+                prev = node
+                size -= 1
+        return root
       
     
